cf68c8f87c2a7de8ab195d19ab6e7dcab223931d
Dmitrii <shokilla@mail.ru>
2022-08-07 14:27:24 +0400

    added another methods

--- server.js
+++ server.js
@@ -6,25 +6,50 @@ const port = 5000;
 
 const _dirname = "/http-course/http-course/files";
 
+const getHundler = (req, res) => {
+  try {
+    const files = fs.readdirSync(_dirname);
+    let fileNames = "";
+    files.forEach((file) => {
+      fileNames += `${file}, `;
+    });
+    res.writeHead(200, "Ok!", { "Content-type": "text/html" });
+    res.end(fileNames);
+    return;
+  } catch (error) {
+    res.writeHead(500, "Ok!", { "Content-type": "text/html" });
+    res.end("Internal server error");
+    return;
+  }
+};
+
 const requestListener = (req, res) => {
-  if (req.url === "/test" && req.method === "GET") {
-    try {
-      const files = fs.readdirSync(_dirname);
-      let fileNames = "";
-      files.forEach((file) => {
-        fileNames += `${file}, `;
-      });
-      res.writeHead(200, "Ok!", { "Content-type": "text/html" });
-      res.end(fileNames);
-      return;
-    } catch (error) {
-      res.writeHead(500, "Ok!", { "Content-type": "text/html" });
-      res.end("Internal server error");
-      return;
-    }
+  if (req.url === "/get" && req.method === "GET") {
+    return getHundler(req, res);
   }
 
-  if (req.url === "/test" && req.method !== "GET") {
+  if (req.url === "/get" && req.method !== "GET") {
+    res.writeHead(405);
+    res.end("HTTP method not allowed");
+    return;
+  }
+
+  if (req.url === "/delete" && req.method === "DELETE") {
+    res.writeHead(200);
+    res.end("Success!");
+    return;
+  }
+  if (req.url === "/delete" && req.method !== "DELETE") {
+    res.writeHead(405);
+    res.end("HTTP method not allowed");
+    return;
+  }
+  if (req.url === "/post" && req.method === "POST") {
+    res.writeHead(200);
+    res.end("Success!");
+    return;
+  }
+  if (req.url === "/post" && req.method !== "POST") {
     res.writeHead(405);
     res.end("HTTP method not allowed");
     return;
